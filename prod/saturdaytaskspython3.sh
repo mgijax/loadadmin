5ac7f231f07165b4a46e6f40a20a99714009eee8
Lori Corbani <lori.corbani@jax.org>
2020-04-15 11:11:56 -0400

    TR13204/Infrastructure/python 3

--- prod/saturdaytaskspython3.sh
+++ prod/saturdaytaskspython3.sh
@@ -31,3 +31,26 @@ scp bhmgiapp01:/data/loads/geo/geoload/output/ACC_Accession.bcp ACC_Accession.bc
 cd /data/loads/geo/geoload/reports
 scp bhmgiapp01:/data/loads/geo/geoload/reports/geoload.rpt geoload.rpt.prod
 
+# genemodelload
+cd /data/loads/mgi/genemodelload/output/
+scp bhmgiapp01:/data/loads/mgi/genemodelload/output/MRK_BiotypeMapping.bcp MRK_BiotypeMapping.bcp.prod
+scp bhmgiapp01:/data/loads/mgi/genemodelload/output/SEQ_GeneModel.ensembl.bcp SEQ_GeneModel.ensembl.bcp.prod
+scp bhmgiapp01:/data/loads/mgi/genemodelload/output/SEQ_GeneModel.ncbi.bcp SEQ_GeneModel.ncbi.bcp.prod
+cd /data/loads/mgi/genemodelload/input
+scp bhmgiapp01:/data/loads/mgi/genemodelload/input/ensembl_biotypes.gz .
+scp bhmgiapp01:/data/loads/mgi/genemodelload/input/ncbi_biotypes.gz .
+scp bhmgiapp01:/data/loads/mgi/genemodelload/input/ensembl.txt .
+cd /data/genemodels/current/models
+scp bhmgiapp01:/data/genemodels/current/models/ensembl_genemodels.txt .
+cd /data/genemodels/pending/associations/ensembl
+scp bhmgiapp01:/data/genemodels/pending//associations/ensembl/ensembl_assoc.txt .
+scp bhmgiapp01:/data/genemodels/pending//associations/ensembl/ensembl_assoc_sanity.rpt ensembl_assoc_sanity.rpt.prod
+scp bhmgiapp01:/data/genemodels/pending//associations/ensembl/ensembl_assoc.txt ensembl_assoc.txt.prod
+scp bhmgiapp01:/data/genemodels/pending//associations/ensembl/ensembl_chr_discrep.rpt ensembl_chr_discrep.rpt.prod
+scp bhmgiapp01:/data/genemodels/pending//associations/ensembl/ensembl_genemodelQC.log ensembl_genemodelQC.log.prod
+scp bhmgiapp01:/data/genemodels/pending//associations/ensembl/ensembl_genemodels_sanity.rpt ensembl_genemodels_sanity.rpt.prod
+scp bhmgiapp01:/data/genemodels/pending//associations/ensembl/ensembl_invalid_marker.rpt ensembl_invalid_marker.rpt.prod
+scp bhmgiapp01:/data/genemodels/pending//associations/ensembl/ensembl_missing_gmid.rpt ensembl_missing_gmid.rpt.prod
+scp bhmgiapp01:/data/genemodels/pending//associations/ensembl/ensembl_reportsWithDiscrepancies.rpt ensembl_reportsWithDiscrepancies.rpt.prod
+scp bhmgiapp01:/data/genemodels/pending//associations/ensembl/ensembl_sec_marker.rpt ensembl_sec_marker.rpt.prod
+
