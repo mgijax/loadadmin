1f1d0c1759421ab8b56210e57e2c17fb2c778583
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2023-03-28 11:53:43 -0400

    updated the *python3 scripts

--- prod/dailytaskspython3.sh
+++ prod/dailytaskspython3.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#
+# if you want to copy the prod outputs to *.prod for later comparison,
+# uncomment the various scp and rsync commands below
+#
+
 if [ "${MGICONFIG}" = "" ]
 then
     MGICONFIG=/usr/local/mgi/live/mgiconfig
@@ -23,31 +28,29 @@ scp bhmgiapp01:/data/downloads/ftp.ncbi.nih.gov/genbank/gb_deletes/gbdel.txt.gz 
 
 # goload
 scp bhmgiapp01:/data/downloads/snapshot.geneontology.org/products/annotations/mgi-prediction.gaf /data/downloads/go_noctua
-scp bhmgiapp01:/data/downloads/snapshot.geneontology.org/products/annotations/noctua_mgi.gpad.gz /data/downloads/go_noctua
-scp bhmgiapp01:/data/downloads/snapshot.geneontology.org/products/annotations/noctua_pr.gpad.gz /data/downloads/go_noctua
-scp bhmgiapp01:/data/downloads/purl.obolibrary.org/obo/uberon.obo data/downloads/purl.obolibrary.org/obo
+scp bhmgiapp01:/data/downloads/snapshot.geneontology.org/products/annotations/mgi.gaf.gz /data/downloads/go_noctua
+scp bhmgiapp01:/data/downloads/purl.obolibrary.org/obo/uberon.obo /data/downloads/purl.obolibrary.org/obo
 scp bhmgiapp01:/data/downloads/purl.obolibrary.org/obo/pr/pr-dev.gpi /data/downloads/purl.obolibrary.org/obo/pr
 scp bhmgiapp01:/data/downloads/ftp.ebi.ac.uk/pub/databases/GO/goa/MOUSE/goa_mouse.gaf.gz /data/downloads/ftp.ebi.ac.uk/pub/databases/GO/goa/MOUSE
 scp bhmgiapp01:/data/downloads/ftp.ebi.ac.uk/pub/databases/GO/goa/MOUSE/goa_mouse.gpi.gz /data/downloads/ftp.ebi.ac.uk/pub/databases/GO/goa/MOUSE
 scp bhmgiapp01:/data/downloads/ftp.ebi.ac.uk/pub/databases/GO/goa/MOUSE/goa_mouse_isoform.gaf.gz /data/downloads/ftp.ebi.ac.uk/pub/databases/GO/goa/MOUSE
 scp bhmgiapp01:/data/downloads/raw.githubusercontent.com/evidenceontology/evidenceontology/master/gaf-eco-mapping-derived.txt /data/downloads/raw.githubusercontent.com/evidenceontology/evidenceontology/master
-scp bhmgiapp01:/data/reports/reports_db/output/mgi.gpa ${PUBREPORTDIR}/output/mgi.gpa.prod
-scp bhmgiapp01:/data/reports/reports_db/output/mgi.gpi ${PUBREPORTDIR}/output/mgi.gpi.prod
-scp bhmgiapp01:/data/reports/reports_db/output/gene_association.mgi ${PUBREPORTDIR}/output/gene_association.mgi.prod
-scp bhmgiapp01:/data/reports/reports_db/output/gene_association_pro.mgi ${PUBREPORTDIR}/output/gene_association_pro.mgi.prod
-rsync -avz bhmgiapp01:/data/loads/go/gomousenoctua/input ${DATALOADSOUTPUT}/go/gomousenoctua/input.prod
-rsync -avz bhmgiapp01:/data/loads/go/gomousenoctua/output ${DATALOADSOUTPUT}/go/gomousenoctua/output.prod
+#scp bhmgiapp01:/data/reports/reports_db/output/mgi.gpa ${PUBREPORTDIR}/output/mgi.gpa.prod
+#scp bhmgiapp01:/data/reports/reports_db/output/mgi.gpi ${PUBREPORTDIR}/output/mgi.gpi.prod
+#scp bhmgiapp01:/data/reports/reports_db/output/gene_association.mgi ${PUBREPORTDIR}/output/gene_association.mgi.prod
+#scp bhmgiapp01:/data/reports/reports_db/output/gene_association_pro.mgi ${PUBREPORTDIR}/output/gene_association_pro.mgi.prod
+#rsync -avz bhmgiapp01:/data/loads/go/gomousenoctua/input ${DATALOADSOUTPUT}/go/gomousenoctua/input.prod
+#rsync -avz bhmgiapp01:/data/loads/go/gomousenoctua/output ${DATALOADSOUTPUT}/go/gomousenoctua/output.prod
 
 # littriageload
-rsync -avz bhmgiapp01:/data/loads/mgi/littriageload/logs ${DATALOADSOUTPUT}/mgi/littriageload/logs.prod
-rsync -avz bhmgiapp01:/data/loads/mgi/littriageload/output ${DATALOADSOUTPUT}/mgi/littriageload/output.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/littriageload/logs ${DATALOADSOUTPUT}/mgi/littriageload/logs.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/littriageload/output ${DATALOADSOUTPUT}/mgi/littriageload/output.prod
 
 # noteload/gotext
-scp bhmgiapp01:/data/loads/go/gotext/input/gotext.txt ${DATALOADSOUTPUT}/go/gotext/input
-rsync -avz bhmgiapp01:/data/loads/go/gotext ${DATALOADSOUTPUT}/go/gotext.prod
+scp bhmgiapp01:/data/downloads/fms.alliancegenome.org/download/GENE-DESCRIPTION-TXT_MGI.txt.gz /data/downloads/fms.alliancegenome.org/download/
 
 # vocload/MP
-rsync -avz bhmgiapp01:/data/loads/mgi/vocload/runTimeMP ${DATALOADSOUTPUT}/mgi/vocload/runTimeMP.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/vocload/runTimeMP ${DATALOADSOUTPUT}/mgi/vocload/runTimeMP.prod
 scp bhmgiapp01:/data/loads/mgi/vocload/runTimeMP/MPheno_OBO.ontology ${DATALOADSOUTPUT}/mgi/vocload/runTimeMP
 scp bhmgiapp01:/data/loads/mgi/vocload/runTimeMP/MP.note ${DATALOADSOUTPUT}/mgi/vocload/runTimeMP
 scp bhmgiapp01:/data/loads/mgi/vocload/runTimeMP/MP.synonym ${DATALOADSOUTPUT}/mgi/vocload/runTimeMP
@@ -56,29 +59,29 @@ scp bhmgiapp01:/data/loads/mgi/vocload/runTimeMP/mp.owl ${DATALOADSOUTPUT}/mgi/v
 
 # vocload/GO
 scp bhmgiapp01:/data/downloads/purl.obolibrary.org/obo/go/snapshot/go-basic.obo /data/downloads/purl.obolibrary.org/obo/go/snapshot
-rsync -avz bhmgiapp01:/data/loads/mgi/vocload/runTimeGO ${DATALOADSOUTPUT}/mgi/vocload/runTimeGO.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/vocload/runTimeGO ${DATALOADSOUTPUT}/mgi/vocload/runTimeGO.prod
 
 # vocload/EMAP
 scp bhmgiapp01:/data/loads/mgi/vocload/emap/input/EMAPA.obo ${DATALOADSOUTPUT}/mgi/vocload/emap/input
-rsync -avz bhmgiapp01:/data/loads/mgi/vocload/emap/input ${DATALOADSOUTPUT}/mgi/vocload/emap/input.prod
-rsync -avz bhmgiapp01:/data/loads/mgi/vocload/emap/output ${DATALOADSOUTPUT}/mgi/vocload/emap/output.prod
-rsync -avz bhmgiapp01:/data/loads/mgi/slimtermload/emapslimload bhmgiapp01:${DATALOADSOUTPUT}/mgi/slimtermload/emapslimload.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/vocload/emap/input ${DATALOADSOUTPUT}/mgi/vocload/emap/input.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/vocload/emap/output ${DATALOADSOUTPUT}/mgi/vocload/emap/output.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/slimtermload/emapslimload bhmgiapp01:${DATALOADSOUTPUT}/mgi/slimtermload/emapslimload.prod
 
 # mcvload
 scp bhmgiapp01:/data/loads/mgi/mcvload/input/mcvload.txt ${DATALOADSOUTPUT}/mgi/mcvload/input/mcvload.txt
 scp bhmgiapp01:/data/loads/mgi/mcvload/input/MCV_Vocab.obo ${DATALOADSOUTPUT}/mgi/mcvload/input/MCV_Vocab.obo
-rsync -avz bhmgiapp01:/data/loads/mgi/mcvload/input ${DATALOADSOUTPUT}/mgi/mcvload/input.prod
-rsync -avz bhmgiapp01:/data/loads/mgi/mcvload/output ${DATALOADSOUTPUT}/mgi/mcvload/output.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/mcvload/input ${DATALOADSOUTPUT}/mgi/mcvload/input.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/mcvload/output ${DATALOADSOUTPUT}/mgi/mcvload/output.prod
 
 # rvload
 scp bhmgiapp01:/data/loads/mgi/rvload/input/RelationshipVocab.obo ${DATALOADSOUTPUT}/mgi/rvload/input
-rsync -avz bhmgiapp01:/data/loads/mgi/rvload/input ${DATALOADSOUTPUT}/mgi/rvload/input.prod
-rsync -avz bhmgiapp01:/data/loads/mgi/rvload/output ${DATALOADSOUTPUT}/mgi/rvload/output.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/rvload/input ${DATALOADSOUTPUT}/mgi/rvload/input.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/rvload/output ${DATALOADSOUTPUT}/mgi/rvload/output.prod
 
 # mrkcoordload - mocked up file
 scp bhmgiapp01:/data/loads/mgi/mrkcoordload/input/mrkcoordload.txt ${DATALOADSOUTPUT}/mgi/mrkcoordload/input
-rsync -avz bhmgiapp01:/data/loads/mgi/mrkcoordload/input ${DATALOADSOUTPUT}/mgi/mrkcoordload/input.prod
-rsync -avz bhmgiapp01:/data/loads/mgi/mrkcoordload/output ${DATALOADSOUTPUT}/mgi/mrkcoordload/output.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/mrkcoordload/input ${DATALOADSOUTPUT}/mgi/mrkcoordload/input.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/mrkcoordload/output ${DATALOADSOUTPUT}/mgi/mrkcoordload/output.prod
 
 # For SEQ_NMs.py
 scp bhmgiapp01:/data/loads/entrezgene/egload/reports/bucket_one_to_many.txt ${DATALOADSOUTPUT}/entrezgene/egload/reports
@@ -87,5 +90,5 @@ scp bhmgiapp01:/data/loads/entrezgene/egload/reports/bucket_many_to_one.txt ${DA
 scp bhmgiapp01:/data/loads/entrezgene/egload/reports/bucket_many_to_many.txt ${DATALOADSOUTPUT}/entrezgene/egload/reports
 
 # mrkcacheload
-rsync -avz bhmgiapp01:/data/loads/mgi/mgicacheload/output ${DATALOADSOUTPUT}/mgi/mgicacheload/output.prod
+#rsync -avz bhmgiapp01:/data/loads/mgi/mgicacheload/output ${DATALOADSOUTPUT}/mgi/mgicacheload/output.prod
 
