a14e0e5981d26a66e585749e70c8de4f2c4dccba
Dave Miers <dave.miers@jax.org>
2023-10-24 07:47:05 -0400

    replace /data/loads with DATALOADSOUTPUT in each path

--- prod/removeLastrunFiles.sh
+++ prod/removeLastrunFiles.sh
@@ -1,32 +1,40 @@
 #!/bin/sh
 
-rm -f /data/loads/arrayexpress/arrayexpload/input/lastrun
-rm -f /data/loads/go/lastrun
-rm -f /data/loads/mgi/curatoralleleload/input/lastrun
-rm -f /data/loads/mgi/curatorbulkindexload/input/lastrun
-rm -f /data/loads/mgi/curatorstrainload/input/lastruncreate
-rm -f /data/loads/mgi/curatorstrainload/input/lastrunupdate
-rm -f /data/loads/mgi/emalload/impc/input/lastrun
-rm -f /data/loads/mgi/fearload/input/lastrun
-rm -f /data/loads/mgi/htmpload/impcmpload/input/lastrun
-rm -f /data/loads/mgi/htmpload/impclaczload.alz/input/lastrun
-rm -f /data/loads/mgi/htmpload/impclaczload/input/lastrun
-rm -f /data/loads/mgi/mcvload/input/lastrun
-rm -f /data/loads/mgi/mrkcoordload/input/lastrun
-rm -f /data/loads/mgi/nomenload/input/lastrun
-rm -f /data/loads/mgi/partnerofload/input/lastrun
-rm -f /data/loads/mgi/problemseqsetload/input/lastrun
-rm -f /data/loads/mgi/qtlarchiveload/input/lastrun
-rm -f /data/loads/mgi/qtlinteractionload/input/lastrun
-rm -f /data/loads/mgi/rvload/input/lastrun
-rm -f /data/loads/mgi/slimtermload/emapslimload/input/lastrun
-rm -f /data/loads/mgi/slimtermload/goslimload/input/lastrun
-rm -f /data/loads/mgi/slimtermload/mpslimload/input/lastrun
-rm -f /data/loads/mgi/strainmarkerload/output/lastrun
-rm -f /data/loads/mgi/uniprotmusfilter/input/lastrun
-rm -f /data/loads/mgi/vocload/emap/input/lastrun
-rm -f /data/loads/mp_hpo/input/lastrun
-rm -f /data/loads/nextprot/nextprotload/input/lastrun
-rm -f /data/loads/pro/proload/input/lastrun
-rm -f /data/loads/swissprot/spseqload/input/lastrun
-rm -f /data/loads/swissprot/trseqload/input/lastrun
+if [ "${MGICONFIG}" = "" ]
+then
+    MGICONFIG=/usr/local/mgi/live/mgiconfig
+    export MGICONFIG
+fi
+
+. ${MGICONFIG}/master.config.sh
+
+rm -f ${DATALOADSOUTPUT}/arrayexpress/arrayexpload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/go/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/curatoralleleload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/curatorbulkindexload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/curatorstrainload/input/lastruncreate
+rm -f ${DATALOADSOUTPUT}/mgi/curatorstrainload/input/lastrunupdate
+rm -f ${DATALOADSOUTPUT}/mgi/emalload/impc/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/fearload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/htmpload/impcmpload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/htmpload/impclaczload.alz/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/htmpload/impclaczload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/mcvload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/mrkcoordload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/nomenload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/partnerofload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/problemseqsetload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/qtlarchiveload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/qtlinteractionload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/rvload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/slimtermload/emapslimload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/slimtermload/goslimload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/slimtermload/mpslimload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/strainmarkerload/output/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/uniprotmusfilter/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mgi/vocload/emap/input/lastrun
+rm -f ${DATALOADSOUTPUT}/mp_hpo/input/lastrun
+rm -f ${DATALOADSOUTPUT}/nextprot/nextprotload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/pro/proload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/swissprot/spseqload/input/lastrun
+rm -f ${DATALOADSOUTPUT}/swissprot/trseqload/input/lastrun
